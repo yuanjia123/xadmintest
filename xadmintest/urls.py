4ef13ded7dc85439fa83d1c045e1fd3e6d73f83e
yuanjia123 <871105356@qq.com>
2020-10-14 21:22:35 +0800

    xadmin 经过几天的安装终于搞好了

--- xadmintest/urls.py
+++ xadmintest/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import xadmin
+
 urlpatterns = [
+    path('student/', include('student.urls')),
     path('admin/', admin.site.urls),
     path('xadmin/', xadmin.site.urls),
 ]
